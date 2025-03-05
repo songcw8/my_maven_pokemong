63b8e465bc4cb1635a46465c7ad3842206a30f66
songcw8 <ben990158@gmail.com>
2025-03-05 16:51:46 +0900

    포켓몬 한국 이름과 도트이미지

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,13 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.Random;
 
 public class main {
@@ -10,8 +15,51 @@ public class main {
         String apiUrl = "https://pokeapi.co/api/v2/pokemon/%d";
         HttpClient client = HttpClient.newHttpClient();
         Random rand = new Random();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl.formatted(rand.nextInt(1,152)))).GET().build();
+        int pokemonId = rand.nextInt(1, 152);
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl.formatted(pokemonId))).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        ObjectMapper mapper = new ObjectMapper();
+//        System.out.println(response.body());
+        Pokemon pokemon = mapper.readValue(response.body(), Pokemon.class);
+        System.out.println(pokemon.sprites.frontDefault);
+
+        String apiUrl2 = "https://pokeapi.co/api/v2/pokemon-species/%d";
+        HttpRequest request2 = HttpRequest.newBuilder().uri(URI.create(apiUrl2.formatted(pokemonId))).GET().build();
+        HttpResponse<String> response2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+
+//        System.out.println(response2.body());
+        PokemonSpecies pokemonSpecies = mapper.readValue(response2.body(), PokemonSpecies.class);
+        // 여러 이름이 나와 -> filter -> language.name == ko -> map -> el.name
+        // 1개 -> findFirst -> Optional (Wrapper) -> 없으면 오류내 (orElseThrow)
+        System.out.println(pokemonSpecies.names
+                .stream()
+                .filter(el -> el.language.name.equals("ko"))
+                .map(el -> el.name)
+                .findFirst().orElseThrow());
     }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true) // 추가
+class Pokemon {
+    @JsonIgnoreProperties(ignoreUnknown = true) // 추가
+    public static class Sprites {
+        @JsonProperty("front_default")
+        public String frontDefault;
+    }
+    public Sprites sprites;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true) // 추가
+class PokemonSpecies {
+    @JsonIgnoreProperties(ignoreUnknown = true) // 추
+    public static class Name {
+        public String name;
+        @JsonIgnoreProperties(ignoreUnknown = true) //
+        public static class Language {
+            public String name;
+        }
+        public Language language;
+    }
+    public List<Name> names;
 }
